1a7022fa6f07865a7a3af23d8293da77d2a82326
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-06-13 11:13:08 +0200

    refactor: Created languages.ts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,10 @@
-import type { Parser, Plugin, Printer, SupportLanguage } from 'prettier';
+import type { Parser, Plugin, Printer } from 'prettier';
 
+import { languages } from './languages.js';
 import { options } from './options.js';
 import { parser } from './parse/index.js';
 import { printer } from './print/index.js';
 import { type NodeType, PARSER_NAME, PRINTER_NAME } from './utils/index.js';
-
-const languages: SupportLanguage[] = [
-  {
-    name: 'Ember Template Tag (gjs)',
-    aliases: ['gjs', 'glimmer-js'],
-    extensions: ['.gjs'],
-    vscodeLanguageIds: ['glimmer-js'],
-    parsers: [PARSER_NAME],
-    group: 'JavaScript',
-  },
-  {
-    name: 'Ember Template Tag (gts)',
-    aliases: ['gts', 'glimmer-ts'],
-    extensions: ['.gts'],
-    vscodeLanguageIds: ['glimmer-ts'],
-    parsers: [PARSER_NAME],
-    group: 'TypeScript',
-  },
-];
 
 const parsers: Record<string, Parser<NodeType>> = {
   [PARSER_NAME]: parser,
