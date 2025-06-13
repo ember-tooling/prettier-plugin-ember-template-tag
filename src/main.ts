46288dd420973601231d23438408b14cd145df42
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-06-13 11:15:31 +0200

    refactor: Used named exports

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,4 @@
-import type { Plugin } from 'prettier';
-
-import { languages } from './languages.js';
-import { options } from './options.js';
-import { parsers } from './parsers.js';
-import { printers } from './printers.js';
-import type { NodeType } from './utils/index.js';
-
-const plugin: Plugin<NodeType> = {
-  languages,
-  parsers,
-  printers,
-  options,
-};
-
-export default plugin;
+export { languages } from './languages.js';
+export { options } from './options.js';
+export { parsers } from './parsers.js';
+export { printers } from './printers.js';
