8054cebefb7cb904129b1e5235421b44b63e9ca0
Liam Potter <liam@liampotter.co.uk>
2025-11-29 19:38:43 +0000

    fix prettier-ignore comments on 3.6.x

--- src/print/index.ts
+++ src/print/index.ts
@@ -154,7 +154,15 @@ function printRawText(
 }
 
 function hasPrettierIgnore(path: AstPath<Node | undefined>): boolean {
-  return path.node?.leadingComments?.at(-1)?.value.trim() === 'prettier-ignore';
+  let possibleComment = path.node?.leadingComments?.at(-1)?.value.trim();
+
+  // @ts-expect-error Comments exist on node sometimes
+  if (!path.node?.leadingComments && path.node?.comments) {
+    // @ts-expect-error Comments exist on node sometimes
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    possibleComment = path.node.comments?.at(-1)?.value.trim();
+  }
+  return possibleComment === 'prettier-ignore';
 }
 
 function checkPrettierIgnore(path: AstPath<Node | undefined>): boolean {
