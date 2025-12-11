04742dd4d7c056f02349ca1c01cc78c15ee6fb7b
Liam Potter <liam@liampotter.co.uk>
2025-12-11 09:34:27 +0000

    Mark dependencies as external

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['content-tag'],
+      external: [
+        'content-tag',
+        'prettier',
+        'prettier/plugins/estree.js',
+        'prettier/plugins/babel.js',
+        '@babel/core',
+      ],
     },
     lib: {
       entry: 'src/main.ts',
